c45c6b056ebb5ea84182b409d99ce37b1ba4ac25
Nandaja Varma <nandaja.varma@gmail.com>
2021-12-16 09:24:33 +0100

    Adding the terraform files to create the cluster

---
+++ terraform/kubernetes.tf
@@ -0,0 +1,27 @@
+# Deploy the actual Kubernetes cluster
+resource "digitalocean_kubernetes_cluster" "kubernetes_cluster" {
+  name    = "do-challenge-cluster"
+  region  = "fra1"
+  version = "1.21.5-do.0"
+
+  tags = ["gitops-k8s-challenge"]
+
+  node_pool {
+    name       = "nodepool-k8s-challenge"
+    size       = "s-2vcpu-4gb"
+    auto_scale = true
+    min_nodes  = 1
+    max_nodes  = 4
+    tags       = ["default-node-pool"]
+    labels = {
+      "app"      = "gitops"
+      "priority" = "high"
+    }
+  }
+}
+
+output "kubeconfig" {
+  value       = digitalocean_kubernetes_cluster.kubernetes_cluster.kube_config.0.raw_config
+  sensitive   = true
+  description = "Kubeconfig of the kubernetes cluster"
+}
---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+}
+
+# Remote state
+terraform {
+  backend "s3" {
+    endpoint = "fra1.digitaloceanspaces.com"
+    region   = "eu-central-1"
+    key      = "terraform.tfstate"
+    bucket   = "k8s-spaces"
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
+}
